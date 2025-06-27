fe5cd65be8d3ab8e575c86b942b6aeff1606945d
eunseozz <eunseolovely@gmail.com>
2025-06-27 23:32:58 +0900

    chore: react-router-dom 설치 및 초기 라우터 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
+import "@/index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import "@/index.css";
-import App from "@/App";
+import routers from "@/routes/routers";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routers} />
   </StrictMode>,
 );
