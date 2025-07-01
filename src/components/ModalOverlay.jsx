10aa8db605e17d63b184509d272ebb5ba98a196b
eunseozz <eunseolovely@gmail.com>
2025-07-02 01:26:01 +0900

    feature: 고정 위치에 표시되는 공통 모달 시스템 구축
    - 특정 DOM을 기준으로 모달을 일관되게 렌더링할 수 있도록 구조화
    - PortalContext를 활용해 전역에서 접근 가능한 타겟 요소 관리
    - 외부 영역 클릭 시 모달을 자동으로 닫기 위한 커스텀 훅 구현
    
    #10

---
+++ src/components/ModalOverlay.jsx
@@ -0,0 +1,68 @@
+import { useRef } from "react";
+import { createPortal } from "react-dom";
+import { IoCloseOutline } from "react-icons/io5";
+import styled from "styled-components";
+
+import useGlobalPortal from "@/hooks/useGlobalPortal";
+import useOnClickOutSide from "@/hooks/useOnClickOutSide";
+
+const ModalOverlay = ({ closeModal, children }) => {
+  const modalRef = useRef(null);
+  const portalContainer = useGlobalPortal();
+
+  useOnClickOutSide(modalRef, closeModal);
+
+  return (
+    portalContainer &&
+    createPortal(
+      <BackScreeen>
+        <ModalContainer ref={modalRef}>
+          <CloseButton
+            onClick={closeModal}
+            title="닫기"
+          >
+            <IoCloseOutline />
+          </CloseButton>
+          {children}
+        </ModalContainer>
+      </BackScreeen>,
+      portalContainer,
+    )
+  );
+};
+
+const BackScreeen = styled.div`
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 99999999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContainer = styled.div`
+  position: relative;
+  background: white;
+  padding: 24px 20px;
+  border-radius: 14px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+  min-width: 430px;
+
+  & > div + div {
+    border-top: 1px solid #eee;
+  }
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 22px;
+  right: 24px;
+
+  svg {
+    font-size: 20px;
+    color: #71717a;
+  }
+`;
+
+export default ModalOverlay;
